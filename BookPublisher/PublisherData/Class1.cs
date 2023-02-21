5073e687c7563d7c21835cada724c15e17b8824d
Juan-Avila92 <jc.avila1092@gmail.com>
2023-02-21 13:20:15 -0500

    Creats DbSet for Authors and Books

--- BookPublisher/PublisherData/Class1.cs
+++ BookPublisher/PublisherData/Class1.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
 using System.Data.Common;
 
 namespace PublisherData
 {
     public class PubContext : DbContext
     {
-
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
