5ab7806552487ed9b6fd724f5bf57b89ce7e1508
Juan-Avila92 <jc.avila1092@gmail.com>
2023-02-21 20:42:33 -0500

    Adds new methods to create a database. It also creates a new author and books. EF generates the relationship between authors and books

--- BookPublisher/PublisherConsole/Program.cs
+++ BookPublisher/PublisherConsole/Program.cs
@@ -1,2 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+
+AddAuthorWithBooks();
+GetAuthorsWithBooks();
+
+void AddAuthorWithBooks()
+{
+    var author = new Author()
+    {
+        FirstName = "J.R.R",
+        LastName = "Tolkien",
+        Books = new List<Book>()
+        {
+            new Book()
+            {
+                Title = "The Silmarillion",
+                PublishDate = new DateTime(1974, 1, 1)
+            },
+             new Book()
+            {
+                Title = "Beren And Luthien",
+                PublishDate = new DateTime(2017, 1, 1)
+            }
+        }
+    };
+
+    using var context = new PubContext();
+
+    context.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthorsWithBooks()
+{
+    using var context = new PubContext();
+
+    var authors = context.Authors.Include(b => b.Books).ToList();
+
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        author.Books.ForEach(book => PrintBookTitle(book.Title));
+    }
+}
+
+void PrintBookTitle(string title)
+{
+    Console.WriteLine("*" + title);
+}
--- BookPublisher/PublisherData/PubContext.cs
+++ BookPublisher/PublisherData/PubContext.cs
@@ -8,5 +8,11 @@ namespace PublisherData
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                    "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
+                );
+        }
     }
 }
